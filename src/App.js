a3fbd96395771c67455b71cc1a9ca1975c17cd1e
Ahmet Avci <ahmetavci.8873@gmail.com>
2022-10-17 14:19:27 +0200

    css-loading

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+           <span className="loading">loading</span> 
+         <div className="innercircle circle1"></div>
+        <div className="innercircle circle2"></div>
+        <div className="innercircle circle3"></div>
+      </div>
+       
+    
+      
+       </div>
   );
 }
 
